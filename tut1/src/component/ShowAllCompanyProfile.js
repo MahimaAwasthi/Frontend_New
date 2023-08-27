ef9ce41f97eb0ff1ce22ab692ad7b906a153e5f6
MahimaAwasthi <40446284+MahimaAwasthi@users.noreply.github.com>
2023-08-27 17:35:46 +0530

    Update ShowAllCompanyProfile.js

--- tut1/src/component/ShowAllCompanyProfile.js
+++ tut1/src/component/ShowAllCompanyProfile.js
@@ -1,75 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Search from './Search';
-import ExcelDownloadButton from './ExcelDownloadButton';
-import { Link } from 'react-router-dom';
+import { useEffect,useState } from "react"
+import axios from "axios"
+import InputField from "./InputField"
+import ExcelDownloadButton from "./ExcelDownloadButton"
 
-export default function ShowAllCompanyProfile() {
+const DisplayAllCompanies = ()=> {
+    const [companyDetail,setAllCompanies] = useState([])
+    const [updatedCompanyDetail,setUpdatedCompanyDetail] = useState({companyName:"",technicalRequirement:"",experience:"",packageOffered:""})
+    const [isEditEnable,setIsEditEnable] = useState()
+    const handleChange = (e) => {
+        setUpdatedCompanyDetail({...updatedCompanyDetail, [e.target.name]: e.target.value})
+        console.log(updatedCompanyDetail)
+      };
 
-   const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = () => {
-    axios.get('http://localhost:8080/company/showAllCompanyProfile',{headers:{
-        'Authorization': `Bearer ${localStorage.getItem("jwtToken")}`
-    }})
-      .then(response => {
-        setData(response.data);
-        console.log("Fetched Data successfully");
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  };
-
-  const handleDelete = (id) => {
-    const parsedId = parseInt(id);
-   
-    axios.delete(`http://localhost:8080/company/deleteCompanyProfile/${parsedId}`,{headers:{
+        useEffect(()=> {
+        axios.get("http://localhost:8081/company/showAllCompanies",{headers:{
             'Authorization': `Bearer ${localStorage.getItem("jwtToken")}`
-        }}).then(response => {
-            console.log('Deleted successfully:', response.data);
-            fetchData();
-          })
-          .catch(error => {
-            console.error('Error deleting item:', error);
-          });
-    }
+        }}).then((res)=> {
+            console.log(res.data)
+            // for(let i =0;i<res.data.length;i++){
+            // console.log(res.data[i])
+            setAllCompanies(res.data)
+            // }
+        }).catch((err) => {
+            console.log(err)
+        })
+    },[companyDetail.length])
     const headers = ['ID', 'Company Name', 'Technical Requirement','Experience','Package Offered'];
-
-
-  return (
-      <div>
-        <ExcelDownloadButton headers={headers} tableData = {data}></ExcelDownloadButton>
-        <Link to="/home" target="_blank">
-        <button>Add Company</button>
-        </Link>
-        <Search></Search>
-        <table>
-        <thead>
-          <tr>
-            {headers.map((header, index) => (
-              <th key={index}>{header}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-        {data.map(item => (
-        <tr key={item.id}>
-         <td> <input type="text" value={item.id} readOnly /></td>
-          <td><input type="text" value={item.companyName} readOnly /></td>
-          <td><input type="text" value={item.technicalRequirement} readOnly /></td>
-          <td><input type="text" value={item.experience} readOnly /></td>
-         <td> <input type="text" value={item.packageOffered} readOnly /></td>
-         <td> <button onClick={() => handleDelete(item.id)}>Delete</button></td>
-         <td> <button >Edit</button></td>
-        </tr>
-      ))}
-      </tbody>
-      </table>
-      </div>
-  );
+const updateDetails = (e) => {
+    axios.put(`http://localhost:8081/company/modifyCompanyProfile/${isEditEnable}`,updatedCompanyDetail,{
+        headers:{
+            'Authorization': `Bearer ${localStorage.getItem("jwtToken")}`
+        }
+    }).then((res) => {
+        console.log(res.data)
+        setIsEditEnable()
+    }).catch((err)=> {
+        console.log(err)
+    })
 }
+
+return(
+    <div>
+        <ExcelDownloadButton headers={headers} tableData = {companyDetail}></ExcelDownloadButton>
+        {
+        companyDetail.map((comp)=> {
+            return(
+        <form >
+            <InputField  id type="text" name="companyName" placeholder="Company Name"  onChange={handleChange} value={isEditEnable === comp.id ? updatedCompanyDetail.companyName :comp.companyName} disabled={isEditEnable === comp.id ? false : true}/>
+            <InputField  type="text" name="technicalRequirement" placeholder="Technical Requirement"  onChange={handleChange} value={isEditEnable === comp.id ? updatedCompanyDetail.technicalRequirement :comp.technicalRequirement} disabled={isEditEnable === comp.id ? false : true}/>
+            <InputField type="text" name="experience" placeholder="Experience"  onChange={handleChange} value={isEditEnable === comp.id ? updatedCompanyDetail.experience :comp.experience} disabled={isEditEnable === comp.id ? false : true}/>
+            <InputField type="text" name="packageOffered" placeholder="Package Offered"  onChange={handleChange} value={isEditEnable === comp.id ? updatedCompanyDetail.packageOffered :comp.packageOffered} disabled={isEditEnable === comp.id ? false : true}/>
+            {
+                isEditEnable === comp.id ?
+                <input type="submit" value="Save" onClick={updateDetails} />
+            :<input type="submit" value="Edit" onClick={(e)=> {
+                e.preventDefault()
+                console.log(comp.id)
+                setIsEditEnable(comp.id)
+                setUpdatedCompanyDetail({companyName:comp.companyName,packageOffered:comp.packageOffered,technicalRequirement:comp.technicalRequirement,experience:comp.experience})
+            }} />
+        }
+            </form>
+            )
+        })}
+    </div>
+)
+}
+
+export default DisplayAllCompanies

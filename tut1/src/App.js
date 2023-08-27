e32251d9931a70538619da08c1fd114dddf1e78a
MahimaAwasthi <40446284+MahimaAwasthi@users.noreply.github.com>
2023-08-27 17:36:13 +0530

    Update App.js

--- tut1/src/App.js
+++ tut1/src/App.js
@@ -1,26 +1,48 @@
 import './App.css';
 import LoginForm from './component/LoginForm';
 import Home from './component/Home';
-import Search from './component/Search';
-import ShowAllCompanyProfile from './component/ShowAllCompanyProfile';
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
-
+import DisplayAllCompanies from './component/DisplayAllCompanies';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [isLoggedIn,setisLoggedIn] = useState(false)
+  useEffect(()=>{
+   
+   axios.get("http://localhost:8081/verify",{headers:{
+    'Authorization': `Bearer ${localStorage.getItem("jwtToken")}`
+   }}).then((res) => {
+    if(res.status === 200) {
+      setisLoggedIn(true)
+    }
+   }).catch((err) => {
+    setisLoggedIn(false)
+    console.log(err)
+   })
   
+    },[isLoggedIn])
   return (
     <div className="App">
      {/* <div>
       <LoginForm/>
      </div> */}
      <Router>
-            
+            { isLoggedIn === true ?
             <Routes>  
                  <Route exact path='/' element={< LoginForm />}></Route>  
                  <Route exact path='/home' element={< Home />}></Route>  
-                 <Route exact path='/showAllCompanyProfile' element={< ShowAllCompanyProfile />}></Route> 
-                 <Route exact path='/search' element={< Search />}></Route>  
+                 <Route exact path='/allCompanies' element={<DisplayAllCompanies/>}></Route>
             </Routes>  
+            :
+            <Routes>  
+                 <Route exact path='/' element={< LoginForm />}></Route>  
+                 <Route exact path='/home' element={< LoginForm />}></Route>  
+                 <Route exact path='/allCompanies' element={<LoginForm/>}></Route>
+            </Routes>  
+            // window.location.replace('http://localhost:3001/')
+
+}
      </Router>
      </div>
   );
